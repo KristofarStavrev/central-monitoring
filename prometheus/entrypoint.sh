d6b90353dd0084f03870764729590ef92ff89628
kristofar.stavrev <kristofur.stavrev@gmail.com>
2025-05-03 18:51:00 +0300

    Fixing prometheus env variables issues

--- prometheus/entrypoint.sh
+++ prometheus/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # Replace env variables in the template and save the final config since you can't do it natively in the prometheus config 
-envsubst < /etc/prometheus/prometheus.yml.template > /etc/prometheus/prometheus.yml
+sed "s|__PROMETHEUS_SCRAPE_TARGET__|$PROMETHEUS_SCRAPE_TARGET|g;
+     s|__PROMETHEUS_AUTH_UNAME__|$PROMETHEUS_AUTH_UNAME|g;
+     s|__PROMETHEUS_AUTH_PASS__|$PROMETHEUS_AUTH_PASS|g" \
+     /prometheus/prometheus.yml.template > /prometheus/prometheus.yml
 # Then run the container command (e.g., prometheus)
 exec "$@"
